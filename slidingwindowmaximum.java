225ffca5e59a45ed7788ef0e966f4c2d71a2de71
PramodAB123 <pramodbkl111@gmail.com>
2025-04-04 18:07:32 +0530

    day one

---
+++ slidingwindowmaximum.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class slidingwindowmaximum {
+    public static void main(String[] args) {
+        Deque<Integer> dq = new ArrayDeque<>();
+        List<Integer> nums = Arrays.asList(1,3,-1,-3,5,3,6,7);
+        int k=3;
+        
+    }
+}
