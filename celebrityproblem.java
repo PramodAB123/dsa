a07c5c6216b4dafd73be622eb91234a1402d56d5
PramodAB123 <pramodbkl111@gmail.com>
2025-04-04 18:10:19 +0530

    day one

---
+++ celebrityproblem.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class celebrityproblem {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int[][] a=new int[4][4];
+        for(int i=0;i<4;i++){
+            for(int j=0;j<4;j++){
+                a[i][j]=sc.nextInt();
+            }
+        }
+        
+    }
+}
