b31f402441c35d3339bd07edbadbd9a2fab6850b
PramodAB123 <pramodbkl111@gmail.com>
2025-04-04 18:20:33 +0530

    day two

--- celebrityproblem.java
+++ celebrityproblem.java
@@ -9,7 +9,23 @@ public class celebrityproblem {
                 a[i][j]=sc.nextInt();
             }
         }
-        
+        // method 1 - brute force
+        // int[] knowme=new int[4];
+        // int[] iknow=new int[4];
+        // for(int i=0;i<4;i++){
+        //     for(int j=0;j<4;j++){
+        //         if(a[i][j]==1){
+        //             knowme[j]++;
+        //             iknow[i]++;
+        //         }
+        //     }
+        // }
+        // for(int i=0;i<4;i++){
+        //     if(knowme[i]==(4-1) && iknow[i]==0){
+        //         System.out.println(i);
+        //     }
+        // }
+
 
     }
 }
