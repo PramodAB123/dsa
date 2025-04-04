03591711ae359184c379494d5dacc28da95514f4
PramodAB123 <pramodbkl111@gmail.com>
2025-04-04 18:50:51 +0530

    day two

--- celebrityproblem.java
+++ celebrityproblem.java
@@ -27,5 +27,36 @@ public class celebrityproblem {
         // }
 
 
+
+        // Method 2 : Optmial
+
+        int top=0, bottom=3;
+        while(top<bottom){  // Changed > to < since we want to continue while top hasn't crossed bottom
+            if(a[top][bottom]==1) top=top+1;
+            else if(a[bottom][top]==1) bottom--;
+            else{
+                bottom--;
+                top++;
+            }
+        }
+        if(top!=bottom) System.out.println(-1); // Changed condition since we want them to meet
+        boolean flag=true;
+        for(int i=0;i<4;i++){
+            if(i==top) continue;
+            if(a[i][top]==1 && a[top][i]==0){ // Fixed condition - celebrity should be known by others (a[i][top]==1) and shouldn't know others (a[top][i]==0)
+                continue;
+            }
+            else{
+                flag=false;
+            }
+        }
+        if(flag){
+            System.out.println(top);
+        }
+        else{
+            System.out.println(-1);
+        }
+
+
     }
 }
