e9aacefef46ce12a5370c2243900da66602e72b1
PramodAB123 <pramodbkl111@gmail.com>
2025-04-13 22:14:49 +0530

    day six

---
+++ firstoccurance.java
@@ -0,0 +1,23 @@
+public class firstoccurance {
+    public static void main(String[] args) {
+        int[] a={5,5,5,6,7,8,9};
+        int k=5;
+        int s=0;
+        int found=-1;
+        int e=a.length-1;
+        while(s<=e){
+            int mid=s+(e-s)/2;
+            if(a[mid]==k){
+                found=mid;
+                e=mid-1;
+            }
+            else if(a[mid]<k){
+                s=mid+1;
+            }
+            else{
+                e=mid-1;
+            }
+        }
+        System.out.println(found);
+    }
+}
