3f2a709a288e09dc0141954b186f9de11ee48c5b
PramodAB123 <pramodbkl111@gmail.com>
2025-04-17 00:06:44 +0530

    day seven

--- largestsubarraywithsum0.java
+++ largestsubarraywithsum0.java
@@ -1,5 +1,23 @@
+import java.util.HashMap;
+
 public class largestsubarraywithsum0 {
     public static void main(String[] args) {
-        
+        int[] p={15,2,-2,3,2,-1,-4,2,-1,-1};
+        HashMap<Integer,Integer> mp=new HashMap<>();
+        int sum=0;
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<p.length;i++){
+            sum+=p[i];
+            if(!mp.containsKey(sum)){
+                mp.put(sum,i);
+            }
+            else {
+                int a=i-mp.get(sum);
+                if(a>max){
+                    max=a;
+                }
+            }
+        }
+        System.out.println(max);
     }
 }
