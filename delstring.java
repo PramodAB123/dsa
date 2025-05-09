6e360f6b0f862d10b6ecc1cdbb82a940baeee6ea
PramodAB123 <pramodbkl111@gmail.com>
2025-05-10 00:54:48 +0530

    first commit

--- delstring.java
+++ delstring.java
@@ -1,5 +1,24 @@
 public class delstring {
-    public static void main(String[] args) {
+    public static String del(String s, int n){
+        // Base case - if we've reached the end of string
+        if(n >= s.length()) {
+            return "";
+        }
         
+        // Build result string recursively
+        String rest = del(s, n+1);
+        
+        // Add current char to result if it's not 'a'
+        if(s.charAt(n) != 'a') {
+            return s.charAt(n) + rest;
+        }
+        return rest;
+    }
+
+    public static void main(String[] args) {
+        String t = "abcax";
+        String s = del(t,0);
+        System.out.println("Original string: " + t);
+        System.out.println("After removing 'a': " + s);
     }
 }
